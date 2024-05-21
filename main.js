b7bec8c41dff5e5baf0e56c0fe15e7bc83d3ce9a
Pedro Macedo Paronetto Faria Viana <pmpviana@gmail.com>
2024-05-21 15:21:08 -0300

    commit final

--- main.js
+++ main.js
@@ -1,25 +1,39 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
+//para
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    
-    // template string
-    const idAudio = `#som_${instrumento}`;
+    const idAudio = `#som_${instrumento}`; //template string
 
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
 
-    tecla.onkeydown = function () {
-        tecla.classList.add('ativa');
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
     }
 
     tecla.onkeyup = function () {
         tecla.classList.remove('ativa');
     }
+
 }
